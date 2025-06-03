a11f085834f6d715e89e751b765ef1fc7cd81824
samyrsn <samyrps44@gmail.com>
2025-06-02 21:59:19 -0300

    3 pastas

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
-function AdicionarTarefa() {
-    console,log("teste aula funcionou");
+const entradaTarefa = document.getElementById('entradaTarefa');
+const listaTarefas = document.getElementById('listaTarefas');
+ 
+let tarefas = [];
+
+function adicionarTarefa() {
+    let texto = entradaTarefa.value.trim();
+    if (texto !="") {
+        console.log(entradaTarefa.value);
+        tarefas.push(entradaTarefa.value);
+        let item = document.createElement('li');
+        item.innerHTML = `<span>${entradaTarefa.value}</spans>`;
+        entradaTarefa.value = "";
+        listaTarefas.appendChild(item);
+    } else {
+        alert("Tarefa inválida");
+    }
+    
 }
+
+entradaTarefa.addEventListener('keypress', function(tecla) {
+    if(tecla.key === 'Enter') {
+        adicionarTarefa();
+    }
+
+});
