758853e54d46a134d1c24d34794f86bb04478176
samyrsn <samyrps44@gmail.com>
2025-06-04 21:08:02 -0300

    3 pasta

--- script.js
+++ script.js
@@ -2,27 +2,32 @@ const entradaTarefa = document.getElementById('entradaTarefa');
 const listaTarefas = document.getElementById('listaTarefas');
  
 let tarefas = JSON.parse(localStorage.getItem('tarefas')) ||  [];
+let posicaoEditar = undefined
 
 carregarTarefas(); 
 
 function adicionarTarefa() {
     let texto = entradaTarefa.value.trim();
     if (texto !="") {
-        tarefas.push(entradaTarefa.value);;
+        if (posicaoEditar != undefined) {
+            tarefas[posicaoEditar] = entradaTarefa.value;
+            posicaoEditar = undefined;
+        } else {
+            tarefas.push(entradaTarefa.value);
+        } 
+
         entradaTarefa.value = "";
         salvarTarefas();
         carregarTarefas();
     } else {
         alert("Tarefa inválida");
-    } 
-    
+    }
 }
 
 entradaTarefa.addEventListener('keypress', function(tecla) {
     if(tecla.key === 'Enter') {
         adicionarTarefa();
     }
-
 });
 
 function carregarTarefas() {
@@ -32,9 +37,9 @@ function carregarTarefas() {
         item.className = "item-lista";
         item.innerHTML = `
         <span class="item">${tarefa}</span>
+        <button id="botaoRemover" onclick="editarTarefa(${posicao})">editar</button>
         <button id="botaoRemover" onclick="removerTarefa(${posicao})">X</button>
         `;
-
         listaTarefas.appendChild(item);
     });
 }
@@ -47,4 +52,9 @@ carregarTarefas();
 
 function salvarTarefas() {
     localStorage.setItem('tarefas',JSON.stringify(tarefas));
+}
+
+function editarTarefa(posicao) {
+    posicaoEditar = posicao;
+    entradaTarefa.value = tarefas[posicao];
 }
